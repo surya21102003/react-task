5df062fe8bda3c8a2e1dc1d034c5933419f24770
surya21102003 <156424142+surya21102003@users.noreply.github.com>
2025-06-26 12:47:05 +0530

    Update ConnectionValidator.jsx

--- task-reactjs/src/components/CanvasArea/ConnectionValidator.jsx
+++ task-reactjs/src/components/CanvasArea/ConnectionValidator.jsx
@@ -1,6 +1,7 @@
-export default (connection, nodes) => {
+const connectionValidator=(connection, nodes) => {
     const sourceNode = nodes.find((node) => node.id === connection.source);
     const targetNode = nodes.find((node) => node.id === connection.target);
     
     return sourceNode?.type === 'blockA' && targetNode?.type === 'blockB';
   };
+export default connectionValidator
